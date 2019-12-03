74a88385af6f4f51295efbef5df76c3f08869773
Ezzeldin Tahoun <15705851+ezzeldinadel@users.noreply.github.com>
2019-12-03 03:09:10 -0500

    Update Federated_Averaging.py

--- Federated_Learning/Federated_Averaging.py
+++ Federated_Learning/Federated_Averaging.py
@@ -53,3 +53,98 @@ for layer in modelJoint.layers:
     weightsJoint = layer.get_weights() # list of numpy arrays
     
 updateUW = weightsJoint - weightsUW
+
+
+
+
+
+
+
+
+####################### Experiment below 
+
+
+
+
+NUM_OF_CLIENTS=3
+NUM_EPOCHS=NUM_OF_EPOS=5
+batch_size = 1000
+FREQ = 300
+
+server_model=create_tehran_model()
+client_models=[]
+clients_weights_for_each_round=[]
+
+#initialize
+
+for epoch in range(NUM_OF_EPOS):
+    clients_weights_for_each_round.append([])
+    
+for epoch in range(NUM_OF_EPOS):
+    for host in range(NUM_OF_CLIENTS): 
+        clients_weights_for_each_round[epoch].append([])
+
+for host in range(NUM_OF_CLIENTS): 
+    client_models.append(create_tehran_model())
+    
+
+val_gen = DataGen(
+    "data/iscx_006d.mat", 
+    idxfilter=lambda x: x % FREQ >= 295, 
+    batch_size=batch_size
+)
+
+
+for epoch in range(NUM_OF_EPOS):
+    print("EPOCH = " + str(epoch))
+    for host in range(NUM_OF_CLIENTS):
+         
+        # fetch model
+        np_model=client_models[host]
+        
+        # load joint weights into the model
+        print("Loading joint weights into host", host)
+        np_model.set_weights(server_model.get_weights())
+
+        # create new generator sequence
+        train_gen = DataGen(
+            "data/iscx_006d.mat", 
+            idxfilter=lambda x: x % FREQ == host * NUM_EPOCHS,
+            batch_size=batch_size
+        )
+
+        # train again for 1 epoch
+        print("Training on host", host)
+        np_model.fit_generator(generator=train_gen, validation_data=val_gen, epochs=1, verbose=True)
+
+        # save snapshot
+        print("Saving weights for host", host)
+        np_model.save_weights("fed_np_mesh_lite/fed_snapshot_host%02d_epoch%02d.h5" % (host, epoch))
+        
+        # save weights
+        print("Save the weights in RAM")
+        clients_weights_for_each_round[epoch][host]=np_model.get_weights()
+    
+        #loss, acc = np_model.evaluate_generator(val_gen)
+        #print("Host " + str(host) + " Model ACC=" + str(acc))
+
+    #server is now summing up the result of this epoch 
+    weights=[]
+    for host in range(NUM_OF_CLIENTS): 
+        weights.append(clients_weights_for_each_round[epoch][host])
+        
+        
+    joint=[] 
+    for i in range(len(weights[0])): 
+            joint.append(0)
+
+    for host in range(NUM_OF_CLIENTS): 
+        for i in range(len(weights[host])): 
+                joint[i] += weights[host][i] #sum up 
+        joint[i] = joint[i]/NUM_OF_CLIENTS
+    server_model.set_weights(joint)
+    
+    loss, acc = server_model.evaluate_generator(val_gen)
+    print("Server Model ACC="+ str(acc))
+        
+
